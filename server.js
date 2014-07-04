f670b74cc0fe5cdfbff0a72621f640854d595b21
alexandros <alexandros@resin.io>
2014-07-04 22:55:52 +0100

    fix a bunch of small issues

--- server.js
+++ server.js
@@ -11,8 +11,6 @@ var out = fs.openSync('./out.log', 'a');
 var err = fs.openSync('./out_err.log', 'a');
 var child = spawn('/usr/bin/sound_start', [], { detached: true, stdio: [ 'ignore', out, err ] });
 
-console.log(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN, process.env.TWILIO_APPLICATION_SID, process.env.PORT)
-
 var app = express();
 var cli = new twilioAPI.Client(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
@@ -23,9 +21,8 @@ app.listen(process.env.PORT);
 cli.account.getApplication(process.env.TWILIO_APPLICATION_SID, function(err, app) {
     if(err) throw err;
     app.register();
-    app.on('incomingSMS', function(smsMessage) {
-        console.log(smsMessage.body)
-        var url = 'http://translate.google.com/translate_tts?tl=en&q=' + encodeURIComponent(smsMessage.body);
+    app.on('incomingSMSMessage', function(smsMessage) {
+        var url = 'http://translate.google.com/translate_tts?tl=en&q=' + encodeURIComponent(smsMessage.Body);
         request(url).pipe(new Lame.Decoder).pipe(new Speaker);
     });
 });
