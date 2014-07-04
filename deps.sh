1e7fd5e58ec7b29746c8cb468132f844b13f9953
alexandros <alexandros@resin.io>
2014-07-04 17:41:41 +0100

    initial commit

---
+++ deps.sh
@@ -0,0 +1,2 @@
+apt-get install -y alsa-utils libasound2-dev
+mv sound_start /usr/bin/sound_start
---
+++ server.js
@@ -0,0 +1,28 @@
+var request = require('request');
+var Lame = require('lame');
+var Speaker = require('speaker');
+var fs = require('fs');
+var spawn = require('child_process').spawn;
+var express = require('express')
+var twilioAPI = require('twilio-api')
+
+// set the volume of the sound out to a reasonable level
+var out = fs.openSync('./out.log', 'a');
+var err = fs.openSync('./out_err.log', 'a');
+var child = spawn('/usr/bin/sound_start', [], { detached: true, stdio: [ 'ignore', out, err ] });
+
+var app = express.createServer();
+var cli = new twilioAPI.Client(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+
+app.use(cli.middleware() );
+app.listen(process.env.PORT);
+
+//Get a Twilio application and register it
+cli.account.getApplication(process.env.TWILIO_APPLICATION_SID, function(err, app) {
+    if(err) throw err;
+    app.register();
+    app.on('incomingSMS', function(smsMessage) {
+        var url = 'http://translate.google.com/translate_tts?tl=en&q=' + encodeURIComponent(smsMessage.Body);
+        request(url).pipe(new Lame.Decoder).pipe(new Speaker);
+    });
+});
