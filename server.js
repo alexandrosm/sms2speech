c4d09122452101c2c9673c57db7a49193628b4ec
nchronas <nchronas@gmail.com>
2015-04-23 12:45:51 +0300

    Create a webserver that receives Twilio notifications and responds correctly

--- server.js
+++ server.js
@@ -10,5 +10,16 @@ var err = fs.openSync('./out_err.log', 'a');
 
 var app = express();
 
-app.use(cli.middleware() );
 app.listen(process.env.PORT);
+
+app.get('/', function (req, res) {
+    var url = 'http://translate.google.com/translate_tts?tl=en&q=' + encodeURIComponent(req.query.Body);
+    request(url).pipe(new Lame.Decoder).pipe(new Speaker);
+
+    var twiml = new twilio.TwimlResponse();
+    res.writeHead(200, {'Content-Type': 'text/xml'});
+    res.end(twiml.toString());
+});
+
+console.log("Hello from resin");
+
